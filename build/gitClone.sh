29da8d51346c06dd72b4938b36d5f1956d2cf49e
anhtuanst1 <anhtuanst1@gmail.com>
2023-08-17 02:09:10 +0700

    update pipeline CI/CD

--- build/gitClone.sh
+++ build/gitClone.sh
@@ -5,4 +5,4 @@
 MainBranch=main
 
 ##---- clone projects ----
-git clone -c user.name=$GitUserName -c user.email=$GitUserEmail --verbose --branch $MainBranch --single-branch  --depth 1 https://5gsb7jmd5zckgp3up2olhr22g7iq2kyy6pkcq7amsnwir72mdhuq@dev.azure.com/tuanphama3-nashtech/sd4780-devops/_git/sd4780_msa app/sd4780_msa
+git clone -c user.name=$GitUserName -c user.email=$GitUserEmail --verbose --branch $MainBranch --single-branch  --depth 1 https://c26n7p7y2vhqspf56jj4o4snc46pkgnn7xdsjz7fmzirwfkvxqwq@dev.azure.com/tuanphama3-nashtech/sd4780-devops/_git/sd4780_msa app/sd4780_msa
